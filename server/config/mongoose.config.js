af37af10d8d21fad1fb9ca99dc4b81d3055af815
ahmadzughaer <ahmadzughaer@gmail.com>
2022-02-12 17:12:33 +0200

    connect to atlas database

--- server/config/mongoose.config.js
+++ server/config/mongoose.config.js
@@ -1,4 +1,4 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/moneyBuddiesDB')
+mongoose.connect(process.env.MyDataBase)
     .then(() => console.log('Established a connection to the database'))
     .catch(err => console.log('Something went wrong when connecting to the database ', err));
